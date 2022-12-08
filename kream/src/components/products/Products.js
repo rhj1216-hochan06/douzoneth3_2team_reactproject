f5991df2e5153f252384f37218ba2dbae55222fd
rhj1216 <rhj1216@gmail.com>
2022-12-08 09:06:01 +0900

    제품 카운트 수정

--- kream/src/components/products/Products.js
+++ kream/src/components/products/Products.js
@@ -1,34 +1,34 @@
 //products.js
 import styles from "./Products.module.css";
 import { Product } from "./product";
-import {useSelector,useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import store from '../../store/store';
-import {up} from '../../store/counterSlice';
+import { up } from '../../store/counterSlice';
 
-export const Products = ({products, setProducts}) => {
+export const Products = ({ products, setProducts }) => {
   const dispatch = useDispatch();
-    const number = useSelector(state=>{
-      return state.counter.value;
-    })
+  const number = useSelector(state => {
+    return state.counter.value;
+  })
   return (
     <>
       Home에서 데이터를 받아 수십개의 상품들 나열
 
-      <button onClick={()=>{
-      dispatch(up(2));
-    }}>+</button> {number};
+      <button onClick={() => {
+        dispatch(up(3));
+      }}>+</button> {number};
 
       <div className={styles.filter}>
         <p>정렬기준 1</p>
         <p>정렬기준 2</p>
         <p>정렬기준 3</p>
       </div>
-      <main className={styles.flex_wrap}>  
-        {products.map((product)=>{ //map을 이용하여 상품 갯수만큼 반복시키기
+      <main className={styles.flex_wrap}>
+        {products.map((product) => { //map을 이용하여 상품 갯수만큼 반복시키기
           return <Product key={`key-${product.id}`} product={product} />;
         })}
       </main>
     </>
-      //map을 이용하여 상품 갯수만큼 반복시키기
+    //map을 이용하여 상품 갯수만큼 반복시키기
   );
 };
