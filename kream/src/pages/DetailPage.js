41c405686cf7ea027f534bbc8e3b317b52bfb3fb
Meat7238 <meat7238@kakao.com>
2022-12-08 15:12:38 +0900

    convertPrice충돌해결

--- kream/src/pages/DetailPage.js
+++ kream/src/pages/DetailPage.js
@@ -1,7 +1,7 @@
 import { Detail } from "../components/detail/Detail.js";
 
 const DetailPage = ({cart, setCart,convertPrice}) => {
-  return <Detail cart={cart} setCart={setCart} />;
+  return <Detail cart={cart} setCart={setCart} convertPrice={convertPrice}/>;
 };
 
 export default DetailPage;
