d142c338c0f91fdc8977633f0d79f1d5d608f134
rhj1216 <rhj1216@gmail.com>
2022-12-08 13:43:06 +0900

    이벤트 배너 추가

--- kream/src/components/eventBanner/eventBanner.jsx
+++ kream/src/components/eventBanner/eventBanner.jsx
@@ -1,20 +1,46 @@
 import styles from "./eventBanner.module.css";
+import { useState }  from 'react';
+
 
 export const EventBanner = () => {
+ 
+  const [count, setCount] = useState(1);
+  const changebanner = (type) => {
+    if(type === "plus") {
+      if(count>2) {
+        setCount(1);
+        return;}
+    setCount(count + 1);
+    return;
+    }
+    else if(type ==="minus"){
+      if(count<2) {
+        setCount(3);
+        return;}
+      setCount(count - 1);
+      return;
+    }
+  }
+
+
   return (
     <article
       className={styles.banner}
       style={{
-        backgroundImage: "url('/images/event1.png')",
+        backgroundImage: "url('/images/event"+`${count}`+".png')",
         backgroundSize: "100%",
       }}
     >
       <div className={styles.right}>
-        <img src="images/icon-swiper-2.svg" alt="right" />
+        <img src="images/icon-swiper-2.svg" alt="right"
+          onClick={() => changebanner("plus")} />
       </div>
       <div className={styles.left}>
-        <img src="images/icon-swiper-1.svg" alt="left" />
+        <img src="images/icon-swiper-1.svg" alt="left"
+          onClick={() => changebanner("minus")} />
       </div>
     </article>
-  );
+   
+ 
+ );
 };
