d05e5889ec195298e23b6228562dd5e1f0d30139
최신형 <tlsgudrhfem@naver.com>
2022-12-08 14:36:19 +0900

    충돌해결

--- kream/src/App.js
+++ kream/src/App.js
@@ -4,25 +4,20 @@ import { Route, Routes, Link } from 'react-router-dom';
 import { TopNavBar } from "./components/header/TopNavBar.js";
 import Home from "./pages/Home";
 import { Products } from "./components/products/Products";
-import { WomenRecommend } from "./components/recommend/womenRecommend.js"; // 여성추천
-import { ManRecommend } from "./components/recommend/manRecommend.js"; // 남성추천
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Provider, useSelector } from 'react-redux';
 import store from './store/store';
 import Login from "./components/loginregister/Login"
 //디테일 페이지
 import DetailPage from './pages/DetailPage';
 import { Detail } from './components/detail/Detail';
 //여성 추천
-//import womenRecommend from './pages/WomenRecommend';
+import womenRecommend from './pages/womenRecommend';
 import { womenRecommend } from './components/recommend/womenRecommend';
 //남성 추천
-//import manRecommend from './pages/ManRecommend';
+import manRecommend from './pages/manRecommend';
 import { manRecommend } from './components/recommend/manRecommend';
 import { Provider, useSelector } from 'react-redux';
-import store from './store/store';
-import Login from "./components/loginregister/Login"
 
 function App() {
   //상품 변수 정의
