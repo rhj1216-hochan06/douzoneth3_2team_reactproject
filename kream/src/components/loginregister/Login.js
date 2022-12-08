b9d70fc35f27917314ab56ee7d25bfe9ebbfde0b
bbbbomin <kbm0225@naver.com>
2022-12-08 20:51:12 +0900

    로그인ui수정

--- kream/src/components/loginregister/Login.js
+++ kream/src/components/loginregister/Login.js
@@ -1,43 +1,54 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import "./loginregister.css"
 
 const Login = () => {
-const [id, setId] =useState("");
+  const [id, setId] = useState("");
 
-const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
 
-const onIdHandler = (event) => {
+  const onIdHandler = (event) => {
     setId(event.currentTarget.value);
   }
 
   const onPasswordHandler = (event) => {
     setPassword(event.currentTarget.value)
-}
-
-const onSubmit = (event) => {
-    event.preventDefault();
-    
   }
 
-return(
-<div className='loginregister'>
-  <div  >
-<form onSubmit={onSubmit}> 
-            <div><input name="id" type="id" placeholder="아이디" value={id} onChange={onIdHandler} class="loginregister__input"/></div>
-            <div><input name="password" type="password" placeholder="비밀번호" value={password} onChange={onPasswordHandler} class="loginregister__input"/></div>
-            <div><button type="submit" class="loginregister__button">로그인</button></div>
+  const onSubmit = (event) => {
+    event.preventDefault();
 
-</form>
+  }
 
-<Link to={`/register`}>
-       <button  class="loginregister__button" >회원가입</button>
-</Link>
-
-</div>
-</div>
+  return (
 
 
-);
+
+    <div className='loginregister'>
+
+<div  >
+
+      <div className="logo">
+            <Link to="/">
+              <img className="logoSize" src="/images/logo.jpg" alt="logo" />
+            </Link>
+          </div>
+       
+        <form onSubmit={onSubmit}>
+          <div><input name="id" type="id" placeholder="아이디" value={id} onChange={onIdHandler} class="loginregister__input" /></div>
+          <div><p className='gulza'>id</p><input name="password" type="password" placeholder="비밀번호" value={password} onChange={onPasswordHandler} class="loginregister__input" /></div>
+          <div><h6 className='gulza'>password</h6><button type="submit" class="loginregister__button">로그인</button></div>
+
+        </form>
+
+        <Link to={`/register`}>
+          <button class="loginregister__button" >회원가입</button>
+        </Link>
+
+    </div>
+
+    </div>
+
+  );
 }
 export default Login;
