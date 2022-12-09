fcb7ac12fe1d3a00438e03788c3e44d5da5f0425
최신형 <tlsgudrhfem@naver.com>
2022-12-09 15:52:53 +0900

    카테고리 변경

--- kream/src/pages/Home.js
+++ kream/src/pages/Home.js
@@ -6,14 +6,14 @@ const Home = ({ products, setProducts }) => {
     <>
     <EventBanner/>
     <Link to="/"> 홈페이지</Link><br/>
-    <Link to="/products"> 상품 목록 보기</Link><br/>
     <Link to="/womenRecommend"> 여성 추천</Link><br/>
     <Link to="/manRecommend"> 남성 추천</Link><br/>
     <Link to="/"> 이벤트</Link><br/>
-    <Link to="/cloth"> 전체 카테고리 (의류)</Link><br/>
-    <Link to="/accessorie"> 전체 카테고리 (패션잡화)</Link><br/>
-    <Link to="/tech"> 전체 카테고리 (테크)</Link><br/>
-    <Link to="/life"> 전체 카테고리 (라이프)</Link><br/>
+    <Link to="/products"> 전체 카테고리 목록 보기</Link><br/>
+    <Link to="/cloth"> 카테고리 (의류)</Link><br/>
+    <Link to="/accessorie"> 카테고리 (패션잡화)</Link><br/>
+    <Link to="/tech"> 카테고리 (테크)</Link><br/>
+    <Link to="/life"> 카테고리 (라이프)</Link><br/>
    </>
   );
 };
