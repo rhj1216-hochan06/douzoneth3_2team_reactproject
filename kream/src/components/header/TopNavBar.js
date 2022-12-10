e6d283c7beb23f09cd33bddd8d19525166fdb693
최신형 <tlsgudrhfem@naver.com>
2022-12-10 19:57:42 +0900

    헤더 수정 완료

--- kream/src/components/header/TopNavBar.js
+++ kream/src/components/header/TopNavBar.js
@@ -1,33 +1,70 @@
 import { Link } from "react-router-dom";
 import styles from "./TopNavBar.module.css";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 export const TopNavBar = ({ cart }) => {
 
   const [search, setSearch] = useState("");
   return (
+    // <header>
+    //   <nav className="navbar navbar-expand-xl nav_area sticky">
+    //     <div className="container">
+    //       <div className={styles.logo}>
+    //         <Link to="/">
+    //           <img className={styles.logoSize} src="/images/logo.jpg" alt="logo" />
+    //         </Link>
+    //       </div>
+    //       <div className={styles.input_wrap}>
+    //         <input className={styles.search} type="text" placeholder="상품을 검색해보세요!"  />&nbsp;
+    //         <Link to="/shop" >
+    //         <img src="/images/icon-search.svg" alt="find" />
+    //         </Link>
+    //       </div>
+    //       <div className=" " id="navbarNav">
+    //         <ul className="navbar-nav ml-auto navbar-center main_menu onepage_nav">
+    //           <li className="nav-item">
+    //             <a className="nav-link" href="/shop">SHOP</a>
+    //           </li>
+    //           <li className="nav-item">
+    //             <a className="nav-link" href="/login">LOGIN</a>
+    //           </li>
+    //           <li className="nav-item">
+    //             {/*                   
+    //             <div className={styles.shopping_cart}>
+    //             <a className="nav-link" href="/cart">CART
+    //             {cart.length >= 1  ? (
+    //               <div className={styles.new_shopping_cart}>
+    //               <p>{cart.length}</p>
+    //               </div>
+    //               ) : (
+    //                 ""
+    //                 )}
+    //               </a> 
+    //               </div>*/}
+    //             <Link to="/cart">
+    //               <div className={styles.shopping_cart}>
+    //                 CART
+    //                 {cart.length >= 1 ? (
+    //                   <div className={styles.new_shopping_cart}>
+    //                     <p>{cart.length}</p>
+    //                   </div>
+    //                 ) : (
+    //                   ""
+    //                 )}
+    //               </div>
+    // //             </Link>
+    //           </li>
+    //         </ul>
+    //       </div>
+    //     </div>
+    //   </nav>
+    //   <div className="nav-bottom"></div>
+    // </header>
     <header>
-      <nav className="navbar navbar-expand-xl nav_area sticky">
-        <div className="container">
-          <div className={styles.logo}>
-            <Link to="/">
-              <img className={styles.logoSize} src="/images/logo.jpg" alt="logo" />
-            </Link>
-          </div>
-          <div className={styles.input_wrap}>
-            <input className={styles.search} type="text" placeholder="상품을 검색해보세요!"  />&nbsp;
-            <Link to="/shop" >
-            <img src="/images/icon-search.svg" alt="find" />
-            </Link>
-          </div>
-          <div className=" " id="navbarNav">
-            <ul className="navbar-nav ml-auto navbar-center main_menu onepage_nav">
-              <li className="nav-item">
-                <a className="nav-link" href="/shop">SHOP</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/login">LOGIN</a>
-              </li>
+      <div className={styles.header_md}>
+        <div className={styles.header_top}>
+          <div className={styles.top_inner}>
+            <ul className={styles.top_list}>
               <li className="nav-item">
                 {/*                   
                 <div className={styles.shopping_cart}>
@@ -46,7 +83,7 @@ export const TopNavBar = ({ cart }) => {
                     CART
                     {cart.length >= 1 ? (
                       <div className={styles.new_shopping_cart}>
-                        <p>{cart.length}</p>
+                        <p className={styles.cart_num}>{cart.length}</p>
                       </div>
                     ) : (
                       ""
@@ -54,11 +91,39 @@ export const TopNavBar = ({ cart }) => {
                   </div>
                 </Link>
               </li>
+              <li className={styles.top_item}>
+                <a href="" className={styles.top_link}>마이페이지</a>
+              </li>
+              <li className={styles.top_item}>
+                <a href="" className={styles.top_link}>로그인</a>
+              </li>
             </ul>
           </div>
         </div>
-      </nav>
-      <div className="nav-bottom"></div>
+        <div className={styles.header_main}>
+          <div className={styles.main_inner}>
+            <h1>
+              <Link to="/">
+                <img className={styles.logo} src="/images/logo.jpg" alt="logo" />
+              </Link>
+            </h1>
+            <div className={styles.gnb_area}>
+              <nav className={styles.gnb}>
+                <ul className={styles.gnb_list}>
+                  <li className={styles.gnb_item}>
+                    <a href="" className={styles.gnb_link}>SHOP</a>
+                  </li>
+                  <li className={styles.search_btn}>
+                    <Link to="/shop" >
+                      <img src="/images/search-icon.svg" alt="find" className={styles.search} />
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
