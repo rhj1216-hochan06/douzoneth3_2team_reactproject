ae26c7b3a6f59ecc0544d268f2e210535dc269e2
bbbbomin <kbm0225@naver.com>
2022-12-11 22:38:19 +0900

    제발회원가입되라

--- kream/src/components/loginregister/register.js
+++ kream/src/components/loginregister/register.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react'
 import "./loginregister.css"
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Register=()=> {
   const [name, setName] = useState("")
   const [id, setId] = useState("");
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
-
+  const [idcheck, setIdcheck] = useState(0)
+  const navigate = useNavigate();
   const onNameHandler = (event) => {
     setName(event.currentTarget.value)
   }
@@ -22,13 +24,78 @@ const Register=()=> {
   const onConfirmPasswordHandler = (event) => {
       setConfirmPassword(event.currentTarget.value)
   }
+const onIdcheck = (event) =>{
+  event.preventDefault()
+  fetch("/api/idcheck",{
+    
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json; charset=utf-8"
+
+    },
+    body: JSON.stringify({
+      "id" : id,
+    })
+
+  })
+    .then((res) => res.json())
+    .then(data => {
+      console.log(data);
+      
+    if(data.user.length!=0) {
+     
+      console.log('아이디확인실패');
+      return alert('중복 아이디 입니다. 다시 입력하세요');
+      
+    }
+    else if(data.user.length==0){
+      console.log('아이디중복확인 sucessss');
+      setIdcheck(1);
+      return alert('사용가능한 아이디 입니다.');
+      
+      };
+    });
+}
+
 
   const onSubmit = (event) => {
     event.preventDefault()
+    
     if(password !== confirmPassword) {
       return alert('비밀번호와 비밀번호확인은 같아야 합니다.')
+    }else{ 
+      if(idcheck===0){
+        return alert('아이디 중복확인이 필요합니다')
+      } else{
+      fetch("/api/register",{
+    
+        method: "POST",
+        headers: {
+          "Content-Type":"application/json; charset=utf-8"
+        },
+        body: JSON.stringify({
+          "userid" : id,
+          "username" : name,
+          "userpw" :password
+        })
+  
+      })
+        .then((res) => res.json())
+        .then(data => {
+          console.log(data);
+          
+        if(data.user.length!=0) {
+          console.log('reg sucessss');
+          return navigate('/')
+        }
+        else if(data.user.length==0){
+          console.log('not sucessss');
+          return alert('미작성한 항목 확인하세요')};
+        });
+      }
     }
-  }
+    }
+  
 
   return (
     <div class="loginregister"  >
@@ -42,7 +109,8 @@ const Register=()=> {
 
       <form onSubmit={onSubmit}>
           <div><div className='gulza'>NAME</div><input name="name" type="text" placeholder="이름" value={name} onChange={onNameHandler} class="loginregister__input"/></div>
-          <div><div className='gulza'>EMAIL</div><input name="email" type="email" placeholder="이메일" value={id} onChange={onIdHandler} class="loginregister__input"/></div>
+          <div><div className='gulza'>id</div><input name="id" type="id" placeholder="아이디" value={id} onChange={onIdHandler} class="loginregister__input"/></div>
+          <div><button type="button" class="loginregister__button" onClick={onIdcheck}>아이디 확인하기</button></div>
           <div><div className='gulza'>PASSWORD</div><input name="password" type="password" placeholder="비밀번호" value={password} onChange={onPasswordHandler} class="loginregister__input"/></div>
           <div><div className='gulza'>PASSWORD CONFIRM</div><input name="confirmPassword" type="password" placeholder="비밀번호 확인" value={confirmPassword} onChange={onConfirmPasswordHandler} class="loginregister__input"/></div>
           <div><button type="submit" class="loginregister__button">계정 생성하기</button></div>
