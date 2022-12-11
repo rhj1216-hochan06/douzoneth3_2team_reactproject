3bdebb300ef127675e6c6624b91673e5931c240a
rhj1216 <rhj1216@gmail.com>
2022-12-12 08:56:45 +0900

    이벤트 배너 링크 연결

--- kream/src/components/eventBanner/eventBanner.jsx
+++ kream/src/components/eventBanner/eventBanner.jsx
@@ -3,16 +3,16 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import "./eventBanner.module.css";
-
+import { Link } from 'react-router-dom';
 export const EventBanner = () => {
   return (
     <>
     <center>
     <Swiper navigation={true} modules={[Navigation]} className="mySwiper" >
-      <SwiperSlide><img src="./images/event.png"/></SwiperSlide>
-      <SwiperSlide><img src="./images/event5.png"/></SwiperSlide>
-      <SwiperSlide><img src="./images/event6.png"/></SwiperSlide>
-      <SwiperSlide><img src="./images/event7.png"/></SwiperSlide>
+      <SwiperSlide><Link to="/products"> <img src="./images/event.png"/></Link></SwiperSlide>
+      <SwiperSlide><Link to="/products"> <img src="./images/event5.png"/></Link></SwiperSlide>
+      <SwiperSlide><Link to="/products"> <img src="./images/event6.png"/></Link></SwiperSlide>
+      <SwiperSlide><Link to="/products"> <img src="./images/event7.png"/></Link></SwiperSlide>
     </Swiper>
     </center>
     </>
