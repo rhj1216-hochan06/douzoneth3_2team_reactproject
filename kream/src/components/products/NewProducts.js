5ca4accd534f3a025ab8f8981a19a9308eae6840
rhj1216 <rhj1216@gmail.com>
2022-12-13 08:23:58 +0900

    products 코드 처리

--- kream/src/components/products/NewProducts.js
+++ kream/src/components/products/NewProducts.js
@@ -8,14 +8,6 @@ import AOS from "aos";
 
 
 export const NewProducts = ({ convertPrice }) => {
-    //----------------------------------------페이징 처리를 위한 카운트
-    const [count, setCount] = useState(1);
-    const increaseNumber = () => { // number의 값을 증가시키는 함수
-        setCount(count + 1);
-        // setNumber(prevNumber => prevNumber + 1);
-        // 다음과 같이 현재 number의 값을 불러와서 증가 시켜주는 방법도 있습니다 :)
-    };
-
     //---------------------------------------------------DAO 시작
     const [state, setState] = useState([]);
 
@@ -40,15 +32,13 @@ export const NewProducts = ({ convertPrice }) => {
     }, []);
 
     const handleScroll = () => {
-        console.log(window.scrollY)
-        // 스크롤이 Top에서 5000px 이상 내려오면 true값을 useState에 넣어줌
-        if (window.scrollY >= 3000) {
-            { increaseNumber(); }
-            console.log(count)
-        } else {
-            // 스크롤이 50px 미만일경우 false를 넣어줌
+        // console.log(window.scrollY)
+        // // 스크롤이 Top에서 5000px 이상 내려오면 true값을 useState에 넣어줌
+        // if (window.scrollY >= 3000) {
+        // } else {
+        //     // 스크롤이 50px 미만일경우 false를 넣어줌
 
-        }
+        // }
 
     };
 
--- kream/src/components/products/Products.js
+++ kream/src/components/products/Products.js
@@ -8,22 +8,6 @@ import AOS from "aos";
 
 
 export const Products = ({ convertPrice }) => {
-  //----------------------------------------페이징 처리를 위한 카운트
-  const [count, setCount] = useState(1);
-  const increaseNumber = () => { // number의 값을 증가시키는 함수
-    setCount(count + 1);
-    // setNumber(prevNumber => prevNumber + 1);
-    // 다음과 같이 현재 number의 값을 불러와서 증가 시켜주는 방법도 있습니다 :)
-  };
-
-  const decreaseNumber = () => { // number의 값을 감소시키는 함수
-    setCount(count - 1);
-    // setNumber(prevNumber => prevNumber - 1);
-    // 다음과 같이 현재 number의 값을 불러와서 감소 시켜주는 방법도 있습니다 :)
-  };
-
-
-
 
   //---------------------------------------------------DAO 시작
   const [state, setState] = useState([]);
@@ -38,32 +22,7 @@ export const Products = ({ convertPrice }) => {
     .then(json => setState(json));
   //--------------------------------------------------------끝
   //----------------------------------------정렬
-  fetch("/api/products/id", {
-    method: "get",
-    headers: {
-      "content-type": "application/json"
-    },
-  })
-    .then((res) => res.json())
-    .then(json => setState(json));
 
-  fetch("/api/products/pricelow", {
-    method: "get",
-    headers: {
-      "content-type": "application/json"
-    },
-  })
-    .then((res) => res.json())
-    .then(json => setState(json));
-
-  fetch("/api/products/pricehigh", {
-    method: "get",
-    headers: {
-      "content-type": "application/json"
-    },
-  })
-    .then((res) => res.json())
-    .then(json => setState(json));
   //--------------------------------------------------------끝
 
   useEffect(() => {
@@ -77,19 +36,16 @@ export const Products = ({ convertPrice }) => {
   }, []);
 
   const handleScroll = () => {
-    console.log(window.scrollY)
-    // 스크롤이 Top에서 5000px 이상 내려오면 true값을 useState에 넣어줌
-    if (window.scrollY >= 3000) {
-      { increaseNumber(); }
-      console.log(count)
-    } else {
-      // 스크롤이 50px 미만일경우 false를 넣어줌
-
-    }
+    // console.log(window.scrollY)
+    // // 스크롤이 Top에서 5000px 이상 내려오면 true값을 useState에 넣어줌
+    // if (window.scrollY >= 3000) {
+    // } else {
+    //   // 스크롤이 50px 미만일경우 false를 넣어줌
+    // }
 
   };
 
-
+  //json 버전 정렬
   // const sortProduct = (type) => {
   //   if (type === "recent") {
   //     const newProduct = [...products];
