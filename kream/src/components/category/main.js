2955eb0887bb05a2760640eb0c5628748b0845fb
최신형 <tlsgudrhfem@naver.com>
2022-12-12 12:22:40 +0900

    전체 카테고리 구현중

--- kream/src/components/category/main.js
+++ kream/src/components/category/main.js
@@ -1,59 +1,29 @@
-import React, { useState } from 'react';
-import styles from './category.module.css';
-import { Products } from '../products/Products';
-import { AccessorieCategory } from './accessorieCategory';
-import { ClothCategory } from './clothCategory';
-import { LifeCategory } from './lifeCategory';
-import { TechCategory } from './techCategory';
+import React from 'react';
+import { Link, Outlet } from "react-router-dom";
 
 export const MainCategory = () => {
-    const [content, setContent] = useState();
-
-    const handleClickButton = e => {
-        const { name } = e.target;
-        setContent(name);
-    };
-
-    const selectComponent = {
-        accessorieCategory: <AccessorieCategory />,
-        clothCategory: <ClothCategory />,
-        lifeCategory: <LifeCategory />,
-        techCategory: <TechCategory />,
-    };
-
-//     const Container = styles.div`
-//     ${props => props.theme.flex('center', 'center')}
-//     height: 20vh;
-//   `;
-
-//     const Button = styles.button`
-//     padding: 1rem 2rem;
-//     margin-right: 1rem;
-//     color: #111111;
-//     background-color: #eeeeee;
-//     border-radius: 2rem;
-//   `;
-
-//     const Content = styles.div`
-//     ${props => props.theme.flex('center', 'center')}
-//     width: 100%;
-//     height: 100%;
-//   `;
-
-    console.log(content);
-
     return (
         <>
             <div>
-                    {Products.map(data => {
-                        return (
-                            <button onClick={handleClickButton} name={data.name} key={data.id}>
-                                {data.text}
-                            </button>
-                        );
-                    })}
-                {content && <div>{selectComponent[content]}</div>}
-            </div>
+                <ul>
+                    <h1>전체 카테고리</h1>
+                    <li>
+                        <Link to="/mainCategory/accrssorieCategory">패션잡화</Link>
+                    </li>
+                    <li>
+                        <Link to="/mainCategory/techCategory">의류</Link>
+                    </li>
+                    <li>
+                        <Link to="/mainCategory/clothCategory">테크</Link>
+                    </li>
+                    <li>
+                        <Link to="/mainCategory/lifeCategory">라이프</Link>
+                    </li>
+                </ul>
+                <hr />
+                <Outlet />
+            </div >
         </>
     );
 };
+
