7c79dc412ab9da461ce5a59e9e30d6a4abd286dc
rhj1216 <rhj1216@gmail.com>
2022-12-12 14:25:18 +0900

    home 쪽 css 수정

--- kream/src/pages/Home.js
+++ kream/src/pages/Home.js
@@ -6,17 +6,28 @@ const Home = () => {
   return (
     <>
       <EventBanner />
-      <Link to="/"> 홈페이지</Link><br />
-      <div className="women">
-        <Link to="/womenRecommend"> < img className={styles.eventimg} src=" /images/recomendwoman.jpg" />여성 추천</Link></div><br />
-      <Link to="/manRecommend">남성 추천</Link><br />
-      <Link to="/"> 이벤트</Link><br />
-      <Link to="/products"> 전체 카테고리 목록 보기</Link><br />
-      <Link to="/cloth"> 카테고리 (의류)</Link><br />
-      <Link to="/accessorie"> 카테고리 (패션잡화)</Link><br />
-      <Link to="/tech"> 카테고리 (테크)</Link><br />
-      <Link to="/life"> 카테고리 (라이프)</Link><br />
-      <Link to="/mainCategory"> 전체 카테고리</Link><br />
+
+      <main className={styles.flex_wrap}>
+        <div className={styles.product}>
+          <Link to="/"> 홈페이지</Link><br />
+
+          <Link to="/womenRecommend">
+            <img className={styles.product_image} src=" /images/recomendwoman.jpg" />
+            <div className={styles.product_name}> 여성 추천</div>
+          </Link><br />
+          <Link to="/manRecommend">
+            <img className={styles.product_image} src=" /images/recomendman.jpg" />
+            <div className={styles.product_name}> 남성 추천</div>
+          </Link> <br />
+          <Link to="/"> 이벤트 </Link><br />
+          <Link to="/products"> 전체 카테고리 목록 보기 </Link><br />
+          <Link to="/cloth"> 카테고리 (의류) </Link><br />
+          <Link to="/accessorie"> 카테고리 (패션잡화) </Link><br />
+          <Link to="/tech"> 카테고리 (테크) </Link><br />
+          <Link to="/life"> 카테고리 (라이프) </Link><br />
+          <Link to="/mainCategory"> 전체 카테고리 </Link><br />
+        </div>
+      </main>
     </>
   );
 };
