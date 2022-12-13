dabe850006432863037933d0be4769e1127100ab
최신형 <tlsgudrhfem@naver.com>
2022-12-13 15:42:04 +0900

    회원가입 css 완료

--- kream/src/components/loginregister/register.js
+++ kream/src/components/loginregister/register.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "./loginregister.css"
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
-const Register=()=> {
+const Register = () => {
   const [name, setName] = useState("")
   const [id, setId] = useState("");
   const [password, setPassword] = useState("")
@@ -21,127 +22,149 @@ const Register=()=> {
   }
 
   const onPasswordHandler = (event) => {
-      setPassword(event.currentTarget.value)
+    setPassword(event.currentTarget.value)
   }
 
   const onConfirmPasswordHandler = (event) => {
-      setConfirmPassword(event.currentTarget.value)
+    setConfirmPassword(event.currentTarget.value)
   }
 
   const onPhonenumberHandler = (event) => {
     setPhonenumber(event.currentTarget.value)
-}
+  }
 
-const onAddressHandler = (event) => {
-  setAddress(event.currentTarget.value)
-}
+  const onAddressHandler = (event) => {
+    setAddress(event.currentTarget.value)
+  }
 
-const onEmailHandler = (event) => {
-  setEmail(event.currentTarget.value)
-}
-const onIdcheck = (event) =>{
-  event.preventDefault()
-  fetch("/api/idcheck",{
-    
-    method: "POST",
-    headers: {
-      "Content-Type":"application/json; charset=utf-8"
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value)
+  }
+  const onIdcheck = (event) => {
+    event.preventDefault()
+    fetch("/api/idcheck", {
 
-    },
-    body: JSON.stringify({
-      "id" : id,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+
+      },
+      body: JSON.stringify({
+        "id": id,
+      })
+
     })
+      .then((res) => res.json())
+      .then(data => {
+        console.log(data);
 
-  })
-    .then((res) => res.json())
-    .then(data => {
-      console.log(data);
-      
-    if(data.user.length!==0) {
-     
-      console.log('아이디확인실패');
-      return alert('중복 아이디 입니다. 다시 입력하세요');
-      
-    }
-    else if(data.user.length===0){
-      console.log('아이디중복확인 sucessss');
-      setIdcheck(1);
-      return alert('사용가능한 아이디 입니다.');
-      
-      };
-    });
-}
+        if (data.user.length !== 0) {
+
+          console.log('아이디확인실패');
+          return alert('중복 아이디 입니다. 다시 입력하세요');
+
+        }
+        else if (data.user.length === 0) {
+          console.log('아이디중복확인 sucessss');
+          setIdcheck(1);
+          return alert('사용가능한 아이디 입니다.');
+
+        };
+      });
+  }
 
 
   const onSubmit = (event) => {
     event.preventDefault()
-    
-    if(password !== confirmPassword) {
+
+    if (password !== confirmPassword) {
       return alert('비밀번호와 비밀번호확인은 같아야 합니다.')
-    }else{ 
-      if(idcheck===0){
+    } else {
+      if (idcheck === 0) {
         return alert('아이디 중복확인이 필요합니다')
-      } else{
-      fetch("/api/register",{
-    
-        method: "POST",
-        headers: {
-          "Content-Type":"application/json; charset=utf-8"
-        },
-        body: JSON.stringify({
-          "userid" : id,
-          "username" : name,
-          "userpw" :password,
-          "useremail" :email,
-          "userphonenumber" : phonenumber,
-          "useraddress" : address
+      } else {
+        fetch("/api/register", {
+
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          },
+          body: JSON.stringify({
+            "userid": id,
+            "username": name,
+            "userpw": password,
+            "useremail": email,
+            "userphonenumber": phonenumber,
+            "useraddress": address
+          })
+
         })
-  
-      })
-        .then((res) => res.json())
-        .then(data => {
-          console.log(data);
-          
-        if(data.user.length!=0) {
-          console.log('reg sucessss');
-          return navigate('/')
-        }
-        else if(data.user.length==0){
-          console.log('not sucessss');
-          return alert('미작성한 항목 확인하세요')};
-        });
+          .then((res) => res.json())
+          .then(data => {
+            console.log(data);
+
+            if (data.user.length != 0) {
+              console.log('reg sucessss');
+              return navigate('/')
+            }
+            else if (data.user.length == 0) {
+              console.log('not sucessss');
+              return alert('미작성한 항목 확인하세요')
+            };
+          });
       }
     }
-    }
-  
+  }
+
 
   return (
     <div class="loginregister2"  >
-      <div>
-    <div className="box" >
-
-<div className="logo">
+      <div className="allRegister">
+        <div className="box" >
+          <br /><br /><br /><br /><br /><br /><br />
+          <div className="logo">
             <Link to="/">
-              <img className="logoSize" src="/images/logo.jpg" alt="logo" />
+              <img className="logoSize" src="/images/logo2.jpg" alt="logo" />
             </Link>
-          </div>
+          </div><br />
+          <form onSubmit={onSubmit}>
+            <div className="size">
+              <div className='gulza'><font color="red" className="point">*</font>NAME</div>
+              <input name="name" type="text" placeholder="이름" value={name} onChange={onNameHandler} class="loginregister__input" />
+            </div><br/>
+            <div className="size">
+              <div className='gulza'><font color="red" className="point">*</font>ID</div>
+              <input name="id" type="id" placeholder="아이디" value={id} onChange={onIdHandler} class="loginregister__input2" />
+              <Button variant="outline-dark" type="button" class="loginregister__button2" onClick={onIdcheck}>중복확인</Button>
+            </div><br/>
+            <div className="size">
+              <div className='gulza2'><font color="red" className="point">*</font>PASSWORD</div>
+              <input name="password" type="password" placeholder="비밀번호" value={password} onChange={onPasswordHandler} class="loginregister__input" />
+            </div><br/>
+            <div className="size">
+              <div className='gulza'><font color="red" className="point">*</font>PASSWORD CONFIRM</div>
+              <input name="confirmPassword" type="password" placeholder="비밀번호 확인" value={confirmPassword} onChange={onConfirmPasswordHandler} class="loginregister__input" />
+            </div><br/>
+            <div className="size">
+              <div className='gulza'><font color="red" className="point">*</font>EMAIL</div>
+              <input name="email" type="email" placeholder="이메일주소" value={email} onChange={onEmailHandler} class="loginregister__input" />
+            </div><br/>
+            <div className="size">
+              <div className='gulza'><font color="red" className="point">*</font>PHONENUMBER</div>
+              <input name="phonenumber" type="text" placeholder="전화번호" value={phonenumber} onChange={onPhonenumberHandler} class="loginregister__input" />
+            </div><br/>
+            <div className="size">
+              <div className='gulza'><font color="red" className="point">*</font>Address</div>
+              <input name="address" type="text" placeholder="주소" value={address} onChange={onAddressHandler} class="loginregister__input" />
+            </div>
+            <div className="size2">
+              <button type="submit" className="loginregister__button">계정 생성하기</button>
+            </div>
+          </form><br/><br/><br/>
+        </div>
+      </div>
+    </div>
 
-      <form onSubmit={onSubmit}>
-          <div><div className='gulza'>NAME</div><input name="name" type="text" placeholder="이름" value={name} onChange={onNameHandler} class="loginregister__input"/></div>
-          <div className='gulza'>ID</div><div><input name="id" type="id" placeholder="아이디" value={id} onChange={onIdHandler} class="loginregister__input2"/><button type="button" class="loginregister__button2" onClick={onIdcheck}>중복확인</button></div>
-          
-          <div><div className='gulza'>PASSWORD</div><input name="password" type="password" placeholder="비밀번호" value={password} onChange={onPasswordHandler} class="loginregister__input"/></div>
-          <div><div className='gulza'>PASSWORD CONFIRM</div><input name="confirmPassword" type="password" placeholder="비밀번호 확인" value={confirmPassword} onChange={onConfirmPasswordHandler} class="loginregister__input"/></div>
-          <div><div className='gulza'>EMAIL</div><input name="email" type="email" placeholder="이메일주소" value={email} onChange={onEmailHandler} class="loginregister__input"/></div>
-          <div><div className='gulza'>PHONENUMBER</div><input name="phonenumber" type="text" placeholder="전화번호" value={phonenumber} onChange={onPhonenumberHandler} class="loginregister__input"/></div>
-          <div><div className='gulza'>Address</div><input name="address" type="text" placeholder="주소" value={address} onChange={onAddressHandler} class="loginregister__input"/></div>
-          <div><button type="submit" class="loginregister__button">계정 생성하기</button></div>
-      </form>
-    </div>
-    
-    </div> 
-    </div>
-    
   );
 }
 export default Register;
