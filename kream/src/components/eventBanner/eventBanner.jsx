f0caa0aeecddc7f4ab92940b3c6a514078e94ff8
최신형 <tlsgudrhfem@naver.com>
2022-12-13 11:37:39 +0900

    메인 베너 반응형 수정 완료

--- kream/src/components/eventBanner/eventBanner.jsx
+++ kream/src/components/eventBanner/eventBanner.jsx
@@ -2,18 +2,18 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
-import "./eventBanner.module.css";
+import styles  from "./eventBanner.module.css";
 import { Link } from 'react-router-dom';
 
 export const EventBanner = () => {
   return (
     <>
     <center>
-    <Swiper navigation={true} modules={[Navigation]} className="mySwiper" >
-      <SwiperSlide><Link to="/products"> <img src="./images/event.png"/></Link></SwiperSlide>
-      <SwiperSlide><Link to="/products"> <img src="./images/event5.png"/></Link></SwiperSlide>
-      <SwiperSlide><Link to="/products"> <img src="./images/event6.png"/></Link></SwiperSlide>
-      <SwiperSlide><Link to="/products"> <img src="./images/event7.png"/></Link></SwiperSlide>
+    <Swiper navigation={true} modules={[Navigation]} className={styles.mySwiper} >
+      <SwiperSlide><Link to="/products"> <img className={styles.img} src="./images/event.png"/></Link></SwiperSlide>
+      <SwiperSlide><Link to="/products"> <img className={styles.img} src="./images/event5.png"/></Link></SwiperSlide>
+      <SwiperSlide><Link to="/products"> <img className={styles.img} src="./images/event6.png"/></Link></SwiperSlide>
+      <SwiperSlide><Link to="/products"> <img className={styles.img} src="./images/event7.png"/></Link></SwiperSlide>
     </Swiper>
     </center>
     </>
